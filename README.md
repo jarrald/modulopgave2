73c957c81aabed0fe5b2f339ec1eb0462812d3f4
Marcus Miyalys <resolutions@riseup.net>
2019-03-09 17:25:26 +0100

    List of lowercased words with no duplicate words, duplicate letters or numbers created, exercise text added and README updated somewhat

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# modulopgave2
+# Modulopgave 2
+
+## Steps
+
+Prerequisites:
+
+- Remove duplicate words
+- Remove words with numbers in them
+- Remove words with repeated letters (apparently we've been informed that a letter must only be there once in the crosswords.)
+- There are no special characters, at least besides æøå
+
+When creating a crosswords puzzle:
+
+- Ensure all letters in the crosswords is only there once.
+- Maybe remove crosswords that are mirrored versions of each other? We should get an equal number of results, because for each crosswords puzzle there's a mirrored version, ie. the number of results * 2 -> equal number.
---
+++ prepare-input-file/PrepareFile.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import java.util.Set;
+import java.util.HashSet;
+
+// For writeCollectionToFile
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.Collection;
+
+// For removeInvalidWords
+import java.util.Iterator;
+
+public class PrepareFile {
+
+  private Set<String> words = new HashSet<>();
+
+  public static void main(String[] args) {
+    new PrepareFile().run();
+  }
+
+  public void run() {
+    lowerCaseRemoveDuplicateWords();
+    removeInvalidWords(words);
+  }
+
+
+  private void writeCollectionToFile(Collection<String> cs, String name) {
+    var fileOut = name;
+    try (
+      var bw = new BufferedWriter(new FileWriter(fileOut));
+    )
+    {
+      for (String c : cs) {
+        bw.write(c + "\n");
+      }
+      bw.flush();
+    }
+    catch (IOException e) { System.out.println(e); }
+  }
+
+  private void printCollection(Collection<String> s) {
+     for (String word : s) {
+       System.out.println(word);
+     }
+  }
+
+  // Removes words with numbers and words with duplicate letters
+  private void removeInvalidWords(Collection<String> cs) {
+    Iterator iter = cs.iterator();
+
+    while ( iter.hasNext() ) {
+      String word = (String) iter.next();
+      char[] splitWord = word.toCharArray();
+      Set<Character> letters = new HashSet<>();
+
+      for (int i = 0; i < splitWord.length; i++) {
+        letters.add(splitWord[i]);
+
+        if ( !Character.isLetter(splitWord[i]) ) {  //isLetter OR !isDigit
+          iter.remove(); // Remove it! 
+          break; // If the word is removed due to any symbol not being a letter, there's no reason to continue looping over the word
+        }
+        else if (letters.size() != i+1) { 
+          iter.remove(); 
+          break;
+        }
+      }
+    }
+    writeCollectionToFile(words, "out3.txt" );
+  }
+
+  private void lowerCaseRemoveDuplicateWords() {
+    var fileIn = "3LetterWord_Unicode.txt";
+    try (
+      var fr = new FileReader(fileIn);
+      var br = new BufferedReader(fr);
+    )
+    {
+
+      while ( br.ready() ) {
+        words.add( br.readLine().toLowerCase() ); // Lowercases and removes duplicates automatically by inserting into a Set
+      }
+      System.out.println( words.size() );
+    }
+    catch (IOException e) { System.out.println(e); }
+  }
+
+}
