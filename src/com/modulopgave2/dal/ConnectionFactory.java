35973e8687460c4dc26c2ebcdb6f6a70c7b59531
dani153i <danielblom90@gmail.com>
2019-03-12 16:50:35 +0100

    Added properties to connection

--- src/com/modulopgave2/dal/ConnectionFactory.java
+++ src/com/modulopgave2/dal/ConnectionFactory.java
@@ -3,12 +3,13 @@ package com.modulopgave2.dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class ConnectionFactory {
-    public static final String URL = "jdbc:mysql://127.0.0.1:3306/";
-    public static final String USER = "root";
-    public static final String PASS = "";
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String USER = "testuser";
+    public static final String PASS = "test";
 
     /**
      * Get a connection to database
@@ -16,6 +17,13 @@ public class ConnectionFactory {
      * @return Connection object
      */
     public static Connection getConnection(String databaseName) throws SQLException {
-        return DriverManager.getConnection(URL + databaseName, USER, PASS);
+
+        Properties properties = new Properties();
+        properties.setProperty("user", USER);
+        properties.setProperty("password", PASS);
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("autoReconnect", "true");
+
+        return DriverManager.getConnection(URL + databaseName, properties);
     }
 }
